cf9ed9b761fa2b7156477c3df7552a758d8358ac
Yu Shiyang <yu.shiyang@gnayihs.uy>
2022-08-26 05:54:20 +0800

    Updated to use authtokens instead of authkey + authemail

--- ddns-cloudflare.sh
+++ ddns-cloudflare.sh
@@ -6,8 +6,7 @@
 
 # Authentication
 zoneid=''
-authkey=''
-authemail=''
+authtoken=''
 # List of domain names to update within the zone
 names=('example.net' 'subdomain.example.net' '*.wildcard.example.net')
 
@@ -18,13 +17,13 @@ publicip=$( curl -s checkip.dyndns.org | cut -d ':' -f 2 | cut -d '<' -f 1 | cut
 echo -e "Detected IP from \e[94mcheckip.dyndns.org\e[39m: \e[97m$publicip\e[39m"
 
 for i in "${names[@]}"; do
-	response=$( curl -s -X GET 'https://api.cloudflare.com/client/v4/zones/'"$zoneid"'/dns_records?type=A&name='"${i}" -H 'Content-Type:application/json' -H 'X-Auth-Key:'"$authkey" -H 'X-Auth-Email:'"$authemail" )
+	response=$( curl -s -X GET 'https://api.cloudflare.com/client/v4/zones/'"$zoneid"'/dns_records?type=A&name='"${i}" -H 'Content-Type:application/json' -H 'Authorization: Bearer '"$authtoken" )
 	recordip=$( echo "$response" | jq -Mr '.result[0].content' )
 	if [ "$recordip" != "$publicip" ]; then
 		recordid=$( echo "$response" | jq -Mr '.result[0].id' )
 		recordttl=$( echo "$response" | jq -Mr '.result[0].ttl' )
 		recordproxied=$( echo "$response" | jq -Mr '.result[0].proxied' )
-		code=$( curl -s -o /tmp/ddns-cloudflare.response -w '%{http_code}' -X PUT 'https://api.cloudflare.com/client/v4/zones/'"$zoneid"'/dns_records/'"$recordid" -H 'Content-Type:application/json' -H 'X-Auth-Key:'"$authkey" -H 'X-Auth-Email:'"$authemail" --data '{"type":"A","name":"'"${i}"'","content":"'"$publicip"'","ttl":'"$recordttl"',"proxied":'"$recordproxied"'}' )
+		code=$( curl -s -o /tmp/ddns-cloudflare.response -w '%{http_code}' -X PUT 'https://api.cloudflare.com/client/v4/zones/'"$zoneid"'/dns_records/'"$recordid" -H 'Content-Type:application/json' -H 'Authorization: Bearer '"$authtoken" --data '{"type":"A","name":"'"${i}"'","content":"'"$publicip"'","ttl":'"$recordttl"',"proxied":'"$recordproxied"'}' )
 		if [ "$code" == 200 ]; then
 			echo -e "200: \e[97m${i}\e[39m updated to $publicip"
 		else
