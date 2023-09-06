cb9a94830b1d3c1470e6238c81e0dd60ad157a9c
Yu Shiyang <yu.shiyang@gnayihs.uy>
2023-09-07 06:13:03 +0800

    Rearranged files, renamed env variable, simplified compose.yaml

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+# Restore writeable volumes
 cp -a /etc/default-crontabs/* /etc/crontabs
 cp -a /var/spool/default-cron/* /var/spool/cron
+
 
 echo "root:ddns-cloudflare@$MAIL_DOMAIN" > /etc/ssmtp/revaliases
 
 crontab="MAILTO=$MAIL_TO"
-crontab="$crontab\n*/$DDNS_CLOUDFLARE_PERIOD * * * * /opt/ddns-cloudflare/ddns-cloudflare $@"
+crontab="$crontab\n*/$DDNS_CLOUDFLARE_INTERVAL_MINS * * * * /opt/ddns-cloudflare/ddns-cloudflare $@"
 echo -e "$crontab" >> /etc/crontabs/root
 
 crond -f -l 4
